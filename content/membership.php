274100ea48b3a9c02992445173081a75722e6098
ShodiqulRadian2020 <oddishodiqul927@gmail.com>
2022-09-04 15:01:15 +0700

    CRUD Membership

--- content/membership.php
+++ content/membership.php
@@ -6,7 +6,7 @@
                         <h2 class="intro-y text-lg font-medium">
                             Transaction - Membership
                         </h2>
-                        <button class="ml-auto btn btn-primary shadow-md mr-2" href="javascript:;" data-tw-toggle="modal" data-tw-target="#new-customer-modal"><i data-lucide='plus-circle' class='w-5 h-5'></i> &nbsp; Registration Membership</button>
+                        <button class="ml-auto btn btn-primary shadow-md mr-2" href="javascript:;" data-tw-toggle="modal" data-tw-target="#new-membership-modal"><i data-lucide='plus-circle' class='w-5 h-5'></i> &nbsp; Registration Membership</button>
                         
                     </div>
                     <div class="grid grid-cols-12 gap-6 mt-5">
@@ -17,6 +17,7 @@
                                         <th>Date</th>
                                         <th>Customer</th>
                                         <th>Payment</th>
+                                        <th>Amount</th>
                                         <th>Join-Expired</th>
                                         <th>Staff</th>
                                         <th width="125px">Action</th>
@@ -29,10 +30,10 @@
                         </div>  
                     </div>
                 </div>
-                <div id="new-customer-modal" class="modal" tabindex="-1" aria-hidden="true">
+                <div id="new-membership-modal" class="modal" tabindex="-1" aria-hidden="true">
                     <div class="modal-dialog">
                         <div class="modal-content">
-                            <form id="create" action="function/customers?menu=create" method="post">
+                            <form id="create" action="function/memberships?menu=create" method="post">
                             <div class="modal-header">
                                 <h2 class="font-medium text-base mr-auto">
                                     New Customer
@@ -40,23 +41,39 @@
                             </div>
                             <div class="modal-body grid grid-cols-12 gap-4 gap-y-3">
                                 <div class="col-span-12">
-                                    <label for="pos-form-1" class="form-label">Name</label>
-                                    <input id="pos-form-1" name="name" type="text" class="form-control flex-1" placeholder="Customer name">
-                                </div>
-                                <div class="col-span-12">
-                                    <label for="pos-form-2" class="form-label">Address</label>
-                                    <textarea id="pos-form-2" name="address" class="form-control flex-1" placeholder="Jl Mayjend Sungkono"></textarea>
-                                </div>
-                                <div class="col-span-12">
-                                    <label for="pos-form-3" class="form-label">Memberhip</label>
-                                    <select for="pos-form-3" class="form-select mt-2 sm:mr-2" name="membership">
-                                    <?php
-                                    $sql = mysqli_query($conn, 'SELECT *FROM membership');
-                                    while ($data = mysqli_fetch_assoc($sql)) {
-                                        echo "<option value='{$data["id"]}'>{$data['name']}</option>";
-                                    }
-                                    ?>
+                                    <label for="pos-form-1" class="form-label">Customer</label>
+                                    <select id="pos-form-1" name="Customer" class="form-control flex-1" require>
+                                        <option value="">--- SELECT CUSTOMER ---</option>
+                                        <?php
+                                        $sql = mysqli_query($conn, "SELECT *FROM Customer WHERE Cust_Status='Y'");
+                                        while ($data = mysqli_fetch_assoc($sql)) {
+                                            echo "<option value='".$data['Cust_No']." - ".$data['Cust_Nama']."'>".$data['Cust_No']." - ".$data['Cust_Nama']."</option>";
+                                        }
+                                        ?>
                                     </select>
+                                </div>
+                                 <div class="col-span-12">
+                                    <label for="pos-form-2" class="form-label">Discount</label>
+                                    <select id="pos-form-2" name="Discount_No" class="form-control flex-1">
+                                        <?php
+                                        $sql = mysqli_query($conn, "SELECT *FROM Discount WHERE Discount_Status='Y'");
+                                        while ($data = mysqli_fetch_assoc($sql)) {
+                                            echo "<option value=".$data['Discount_No'].">".$data['Discount_Nama']." - ".$data['Persentase']."%</option>";
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <div class="col-span-12">
+                                    <label for="pos-form-3" class="form-label">Jumlah Pemabayaran</label>
+                                    <input id="pos-form-3" type="text" name="Registrasi_Payment" class="form-control flex-1" placeholder="Jumlah Pembayaran">
+                                </div>
+                                <div class="col-span-6">
+                                    <label for="pos-form-4" class="form-label">Start Date</label>
+                                    <input id="pos-form-4" type="date" name="Cust_Member_Join" class="form-control flex-1" onchange="fillExpired(this.valueAsDate)">
+                                </div>
+                                <div class="col-span-6">
+                                    <label for="expired" class="form-label">Expired Date</label>
+                                    <input id="expired" type="date" name="Cust_Member_Exp" class="form-control flex-1" readonly>
                                 </div>
                             </div>
                             <div class="modal-footer text-right">
@@ -106,20 +123,48 @@
                         </div>
                     </div>
                 </div>
+                <div id="payment-member-modal" class="modal" tabindex="-1" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <form id="payment" action="function/memberships?menu=payment" method="post">
+                            <div class="modal-header">
+                                <h2 class="font-medium text-base mr-auto">
+                                    Payment Update
+                                </h2>
+                            </div>
+                            <div class="modal-body grid grid-cols-12 gap-4 gap-y-3">
+                                <div class="col-span-12">
+                                    <label for="payment-status" class="form-label">Payment Status</label>
+                                    <input id="registrasi-id" type="hidden" name="Registrasi_ID">
+                                    <select id="payment-status" name="Status_Payment" class="form-control flex-1">
+                                        <option value="N">UNPAID</option>
+                                        <option value="Y">PAID</option>
+                                    </select>
+                                </div>
+                                <div class="col-span-12">
+                                    <label for="payment-type" class="form-label">Payment Type</label>
+                                    <select id="payment-type" name="Payment_Type" class="form-control flex-1">
+                                        <option value="CASH">CASH</option>
+                                        <option value="TRANSFER">TRANSFER</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="modal-footer text-right">
+                                <button type="button" data-tw-dismiss="modal" class="btn btn-outline-secondary w-32 mr-1">Cancel</button>
+                                <button type="button" onclick="document.getElementById('payment').submit()" class="btn btn-primary w-32">Save</button>
+                            </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
                 <!-- END: Content -->
             </div>
             <?php include 'appjs.php'; ?>
-            <script src="plugin/datatable/jquery-3.5.1.js"></script>
-            <script type="text/javascript" src="plugin/datatable/jquery.dataTables.min.js"></script>
-            <script type="text/javascript" src="plugin/datatable/dataTables.buttons.min.js"></script>
-            <script type="text/javascript" src="plugin/datatable/buttons.flash.min.js"></script>
-            <script type="text/javascript" src="plugin/datatable/jszip.min.js"></script>
-            <script type="text/javascript" src="plugin/datatable/buttons.html5.min.js"></script>
             <script type="text/javascript">
                 $(document).ready(function() {
                     $('#example').DataTable( {
                         "ajax":{
-                            url :"content/ajax/membership_processing.php",
+                            url :"function/memberships?menu=data",
                             type: "post",
                             error: function(){
                                 $(".dataku-error").html("");
@@ -133,10 +178,8 @@
                         buttons: [
                             'excel',
                         ],   
-                    } );     
-                } );
-
-              
+                    });     
+                });
 
                 setTimeout(function(){ 
 
@@ -151,7 +194,50 @@
 
                 }, 500);
 
+                function btnEdit(id) {
+                    $.ajax({
+                        url : "function/memberships?menu=ajax",
+                        type: "post",
+                        data: "Discount_No="+id,
+                            success: function(data) { // Jika berhasil
+                                var json = data,
+                                obj = JSON.parse(json);
+                                $('#edit-id').val(obj.Discount_No);
+                                $('#edit-name').val(obj.Discount_Nama);
+                                $('#edit-type').val(obj.Discount_Type);
+                                $('#edit-persentase').val(obj.Persentase);
+                            }
+                    });
+                }    
+                function btnDelete(id) {
+                    location.href = "function/discounts?menu=delete&id="+id;
+                } 
                 
+                function fillExpired(value){
+                    var expired = value.setDate(value.getDate() + 30); 
+                    
+                    // if (edit == null) {
+                        document.getElementById('expired').value = new Date(value).toISOString().split('T')[0];
+                    // } else {
+                    //     document.getElementById('edit-expired').value = expired;
+                    // }
+                }
+
+                function btnPayment(id) {
+                     $.ajax({
+                        url : "function/memberships?menu=get_payment",
+                        type: "post",
+                        data: "Registrasi_ID="+id,
+                            success: function(data) { // Jika berhasil
+                                var json = data,
+                                obj = JSON.parse(json);
+                                $('#registrasi-id').val(obj.Registrasi_ID);
+                                $('#payment-status').val(obj.Status_Payment);
+                                $('#payment-type').val(obj.Payment_Type);
+                            }
+                    });
+                }    
+            </script>
             </script>
 
             
---
+++ function/memberships.php
@@ -0,0 +1,147 @@
+<?php
+session_start();
+date_default_timezone_set("Asia/Jakarta");
+include "../config/configuration.php";
+
+if ($_GET['menu'] == 'create' ) {
+	// ADD NEW CUSTOMER
+	$Customer 		    = explode(' - ', $_POST['Customer']);
+	$Cust_No 		    = $Customer[0];
+	$Cust_Nama 		 	= $Customer[1];
+	$Registrasi_Tgl 	= date('Y-m-d');
+    $Discount_No 		= $_POST['Discount_No'];
+	$Registrasi_Payment = $_POST['Registrasi_Payment'];
+    $Cust_Member_Join 	= $_POST['Cust_Member_Join'];
+	$Cust_Member_Exp 	= $_POST['Cust_Member_Exp'];
+    $Staff_Name 		= ($_SESSION <> null) ? $_SESSION['Staff_Name'] : 'Admin';
+	$Staff_ID 		    = ($_SESSION <> null) ? $_SESSION['Staff_ID'] : 'Admin';
+
+	mysqli_query($conn, "INSERT INTO Registrasi_Member (Cust_No, Cust_Nama, Registrasi_Tgl, Discount_No, Registrasi_Payment, Cust_Member_Join, Cust_Member_Exp, Staff_ID) VALUES ('$Cust_No', '$Cust_Nama', '$Registrasi_Tgl', '$Discount_No', '$Registrasi_Payment', '$Cust_Member_Join', '$Cust_Member_Exp', '$Staff_ID')");
+
+} elseif ($_GET['menu'] == 'edit') {
+    // UPDATE EXISTING CUSTOMER
+	$id 		 	= $_POST['id'];
+	$name 		 	= $_POST['name'];
+	$discount 		= $_POST['discount'];
+	
+    mysqli_query($conn, "UPDATE membership SET name='$name', discount='$discount' WHERE id='$id'");
+} elseif ($_GET['menu'] == 'data') {
+    $requestData= $_REQUEST;
+    $columns = array( 
+        0 =>'Cust_No',
+        1 =>'Cust_Nama',
+        2 =>'Registrasi_Tgl',
+        3 =>'Discount_No',
+        4 =>'Registrasi_Payment',
+        5 =>'Status_Payment',
+        6 =>'Payment_Type',
+        7 =>'Cust_Member_Join',
+        8 =>'Cust_Member_Exp',
+        9 =>'Staff_Name',
+    );
+    //----------------------------------------------------------------------------------
+    //join 2 tabel dan bisa lebih, tergantung kebutuhan
+    $sql = "SELECT * from Registrasi_Member";
+    $query=mysqli_query($conn, $sql) or die("membership_processing.php: get dataku");
+    $totalData = mysqli_num_rows($query);
+    $totalFiltered = $totalData;
+
+    //----------------------------------------------------------------------------------
+    $sql = " SELECT * from Registrasi_Member";
+    $sql.= " WHERE 1=1";
+    if( !empty($requestData['search']['value']) ) {
+        //----------------------------------------------------------------------------------
+        $sql.=" AND Cust_Nama NOT LIKE '[DELETE]%' AND ( Cust_Nama LIKE '%".$requestData['search']['value']."%' ";    
+        $sql.=" OR Registrasi_Tgl LIKE '".$requestData['search']['value']."%' ";
+        $sql.=" OR Payment_Type LIKE '".$requestData['search']['value']."%' ";
+        $sql.=" OR Registrasi_Payment LIKE '%".$requestData['search']['value']."%' ";
+        $sql.=" OR Cust_Member_Join LIKE '%".$requestData['search']['value']."%' ";
+        $sql.=" OR Cust_Member_Exp LIKE '".$requestData['search']['value']."%' )";
+    }
+    //----------------------------------------------------------------------------------
+    $query=mysqli_query($conn, $sql) or die("membership_processing.php: get dataku");
+    $totalFiltered = mysqli_num_rows($query);
+    $sql.=" ORDER BY ". $columns[$requestData['order'][0]['column']]."   ".$requestData['order'][0]['dir']."  LIMIT ".$requestData['start']." ,".$requestData['length']."   ";  
+    $query=mysqli_query($conn, $sql) or die("membership_processing.php: get dataku");
+    //----------------------------------------------------------------------------------
+    $data = array();
+    while( $row=mysqli_fetch_array($query) ) {
+        $amount = number_format($row['Registrasi_Payment'],0,',','.');
+        
+        $nestedData=array(); 
+        $nestedData[] = $row['Registrasi_Tgl'];
+        $nestedData[] = $row["Cust_Nama"];
+        $nestedData[] = ($row['Status_Payment'] == 'Y') ? "<button class='btn btn-sm btn-dark w-16 mr-1 mb-2' data-tw-toggle='modal' data-tw-target='#payment-member-modal' onclick='btnPayment(".$row['Registrasi_ID'].")'>PAID</button>" : "<button class='btn btn-sm btn-danger w-16 mr-1 mb-2' data-tw-toggle='modal' data-tw-target='#payment-member-modal' onclick='btnPayment(".$row['Registrasi_ID'].")'>UNPAID</button>";
+        $nestedData[] = ($row['Status_Payment'] == 'Y') ? $amount." | ".$row["Payment_Type"] : "";
+        $nestedData[] = $row["Cust_Member_Join"]." - ".$row["Cust_Member_Exp"];
+        $nestedData[] = $row["Staff_Name"];
+        $nestedData[] = "<button class='btn btn-sm btn-primary w-16 mr-1 mb-2'>PRINT</button>";
+        
+        $data[] = $nestedData;
+    }
+    //----------------------------------------------------------------------------------
+    $json_data = array(
+                "draw"            => intval( $requestData['draw'] ),  
+                "recordsTotal"    => intval( $totalData ), 
+                "recordsFiltered" => intval( $totalFiltered ), 
+                "data"            => $data );
+    //----------------------------------------------------------------------------------
+    echo json_encode($json_data);
+    exit();
+
+} elseif ($_GET['menu'] == 'ajax') { 
+    // KEYWORD DIKIRIM DENGAN METHOD POST
+    $Registrasi_ID = $_POST['Registrasi_ID'];
+
+    $sql 	= mysqli_query($conn, "SELECT * FROM Registrasi_Member WHERE Registrasi_ID='$Registrasi_ID'");
+    $data 	= mysqli_fetch_assoc($sql);
+
+    $json = [ 
+        'Registrasi_ID'         => $data['Registrasi_ID'],
+        'Customer'              => $data['Cust_No'] .' - '.$data['Cust_Nama'],
+        'Registrasi_Tgl'        => $data['Registrasi_Tgl'],
+        'Discount_No'           => $data['Discount_No'],
+        'Registrasi_Payment'    => $data['Registrasi_Payment'],
+        'Status_Payment'        => $data['Status_Payment'],
+        'Payment_Type'          => $data['Payment_Type'],
+        'Cust_Member_Join'      => $data['Cust_Member_Join'],
+        'Cust_Member_Exp'       => $data['Cust_Member_Exp'],
+        'Staff_Name'            => $data['Staff_Name'],
+    ];
+    // MERUBAH VARIABEL ARRAY KE FORMAT JSON
+    echo json_encode($json);
+    exit();
+} elseif ($_GET['menu'] == 'get_payment') { 
+    // KEYWORD DIKIRIM DENGAN METHOD POST
+    $Registrasi_ID = $_POST['Registrasi_ID'];
+
+    $sql 	= mysqli_query($conn, "SELECT * FROM Registrasi_Member WHERE Registrasi_ID='$Registrasi_ID'");
+    $data 	= mysqli_fetch_assoc($sql);
+
+    $json = [ 
+        'Registrasi_ID'         => $data['Registrasi_ID'],
+        'Status_Payment'        => $data['Status_Payment'],
+        'Payment_Type'          => $data['Payment_Type'],
+    ];
+    // MERUBAH VARIABEL ARRAY KE FORMAT JSON
+    echo json_encode($json);
+    exit();
+} elseif ($_GET['menu'] == 'payment') { 
+    // UPDATE PAYMENT STATUS
+    $Registrasi_ID  = $_POST['Registrasi_ID'];
+    $Status_Payment = $_POST['Status_Payment'];
+    $Payment_Type   = $_POST['Payment_Type'];
+    $Staff_Name 	= ($_SESSION <> null) ? $_SESSION['Staff_Name'] : 'Admin';
+	$Staff_ID 		= ($_SESSION <> null) ? $_SESSION['Staff_ID'] : 'Admin';
+
+    mysqli_query($conn, "UPDATE Registrasi_Member SET Status_Payment='$Status_Payment', Payment_Type='$Payment_Type', Staff_ID='$Staff_ID', Staff_Name='$Staff_Name' WHERE Registrasi_ID='$Registrasi_ID'");
+
+} else {
+	// DELETE CUSTOMER
+	$Registrasi_ID = $_GET['id'];
+
+	mysqli_query($conn, "DELETE FROM Registrasi_Member WHERE Registrasi_ID='$Registrasi_ID'");
+}
+// REDIRECT KEMBALI KE HALAMAN USER
+echo "<script>location.href='../app?p=membership';</script>";
+?>
