83edf5fd07b925bbd26b0e99dc9ffe3457f8ddf7
ShodiqulRadian2020 <oddishodiqul927@gmail.com>
2022-09-04 11:07:05 +0700

    move script js to appjs

--- appjs.php
+++ appjs.php
@@ -1,1 +1,7 @@
-<script src="dist/js/app.js"></script>
+<script type="text/javascript" src="dist/js/app.js"></script>
+<script type="text/javascript" src="plugin/datatable/jquery-3.5.1.js"></script>
+<script type="text/javascript" src="plugin/datatable/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="plugin/datatable/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="plugin/datatable/buttons.flash.min.js"></script>
+<script type="text/javascript" src="plugin/datatable/jszip.min.js"></script>
+<script type="text/javascript" src="plugin/datatable/buttons.html5.min.js"></script>
