a0aa5513cd9d32f23142fefafe2d5d273bb08301
oddishodiqul <oddishodiqul927@gmail.com>
2022-11-10 00:38:28 +0700

    logo

--- layout/head.php
+++ layout/head.php
@@ -1,5 +1,5 @@
         <meta charset="utf-8">
-        <link href="dist/images/logo.svg" rel="shortcut icon">
+        <link href="dist/logowhite.png" rel="shortcut icon">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="Icewall admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
         <meta name="keywords" content="admin template, Icewall Admin Template, dashboard template, flat admin template, responsive admin template, web app">
