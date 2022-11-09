1f8e01c627a740227c100aa928b1eec023aa30a5
rafial2309 <71589425+rafial2309@users.noreply.github.com>
2022-11-09 13:31:18 +0700

    Configuration

--- config/configuration.php
+++ config/configuration.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('mycherreelaundry.com','u1738884_pos_user','Kosong31!','u1738884_pos_mycherree');
+$conn = mysqli_connect('mycherreelaundry.com','u1738884_pos','Kosong31!','u1738884_pos');
 
 ?>
